68f1e9f5cf5128f67abaffc5ed90c4844d0ee386
ALKALiKong <54100342+ALKALiKong233@users.noreply.github.com>
2020-02-05 15:48:52 +0800

    dipper: Build updatable_apex only in gapps build

--- device.mk
+++ device.mk
@@ -6,7 +6,9 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+ifeq ($(CURRENT_BUILD_TYPE), gapps)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+endif
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/dipper/dipper-vendor.mk)
