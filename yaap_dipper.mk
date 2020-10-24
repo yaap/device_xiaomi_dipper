e7c943f11eed994a18abbd22475a64a1759ddf69
ALKALiKong <alikongalk@gmail.com>
2020-10-24 08:42:24 +0800

    dipper: Update fingerprint form
    Signed-off-by: ALKALiKong <alikongalk@gmail.com>

--- yaap_dipper.mk
+++ yaap_dipper.mk
@@ -16,10 +16,15 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys"
+# Build fingerprint
+BUILD_FINGERPRINT := Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys
+BUILD_DESC := dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys" \
-    PRODUCT_NAME="dipper"
+    PRODUCT_NAME="dipper" \
+    PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
